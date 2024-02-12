681b11cab56ae616ed71d6fc4afffdc6b8b0f90a
Ternario <andrii.shevchenko.6.9.6@gmail.com>
2024-02-12 17:26:42 +0100

    Todo-version-2

---
+++ src/components/Todos/TodosActions.jsx
@@ -0,0 +1,22 @@
+import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri";
+import Button from "../UI/Button";
+import styles from "./TodoActions.module.css";
+
+function TodoActions({ resetTodos, deletCompletedTodos, completedTodosExist }) {
+    return (
+        <div className={styles.todosActionsContainer}>
+            <Button title="Reset Todos" onClick={resetTodos}>
+                <RiRefreshLine />
+            </Button>
+            <Button
+                title="Clear Completed Todos"
+                onClick={deletCompletedTodos}
+                disabled={!completedTodosExist}
+            >
+                <RiDeleteBin2Line />
+            </Button>
+        </div>
+    );
+}
+
+export default TodoActions;
